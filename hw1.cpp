e4fd77a383de9bd89f42cba949df55f57fd03fdd
chadraymor <77703531+chadraymor@users.noreply.github.com>
2021-02-23 17:29:42 -0600

    Add files via upload

---
+++ hw1.cpp
@@ -0,0 +1,136 @@
+/*******************************************************
+ * Homework 1: Rasterization                           *
+ *-----------------------------------------------------*
+ * Here you will implement the circle rasterization    *
+ * method decribed in the handout.                     *
+ * To compile this in linux:                           *
+ *        g++ hw1.cpp                                  *
+ * Then, run the program as follows:                   *
+ *        ./a.out 200                                  *
+ * to generate a 200x200 image containing a circular   *
+ * arc.  Note that the coordinate system we're using   *
+ * places pixel centers at integer coordinates and     *
+ * has the origin at the lower left.                   *
+ * Your code will generate a .ppm file containing the  *
+ * final image. These images can be viewed using       *
+ * "display" in Linux or Irfanview in Mac/Windows.     *
+ *******************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cassert>
+using namespace std;
+
+// We'll store image info as globals; not great programming practice
+// but ok for this short program.
+int radius;
+bool **image;
+
+void renderPixel(int x, int y, bool type) {
+    assert(x >= 0 && y >= 0 && x <= radius && y <= radius);
+
+    int cx = radius/2;
+    int cy = radius/2;
+
+    // TODO:  light up the pixel's symmetric counterpart
+    image[cx][cy] = 1;
+    //right top
+    image[cx+y][cy+x] = 1;
+    //right top-mid
+    image[cx+x][cy+y] = 1;
+
+    if(type==1)
+    {
+        //right bottom-mid
+        image[cx-x][cy+y] = 1;
+        //right bottom
+        image[cx-y][cy+x] = 1;
+    }
+
+    if(type==0)
+    {
+        //left top-mid
+        image[cx+x][cy-y] = 1;
+        //left top
+        image[cx+y][cy-x] = 1;
+    }
+}
+
+void rasterizeArc(int r) 
+{
+    // TODO:  rasterize the arc using renderPixel to light up pixels
+    
+    //type determines which segments of the circle light up (big semi or smaller semi)
+    bool type;
+    if(r<radius/2)
+        type=1;
+    else
+        type=0;
+
+    
+    int x = 0;
+    int y = r;
+    //decision varaible
+    int d = 1 - r;
+
+    renderPixel(x, y, type);
+    while (y >= x) 
+    {
+        x++;
+        if (d < 0)
+            d += 2 * x + 3;
+        else {
+            d += 2 * (x - y) + 5;
+            y--;
+        }
+        renderPixel(x, y, type);
+    }
+}
+
+// You shouldn't need to change anything below this point.
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " circleSize\n";
+        return 0;
+    }
+
+#ifdef _WIN32
+    sscanf_s(argv[1], "%d", &radius);
+#else
+    sscanf(argv[1], "%d", &radius);
+#endif
+    if (radius <= 0) {
+        cout << "Image must be of positive size.\n";
+        return 0;
+    }
+
+    // reserve image as 2d array
+    image = new bool*[radius+1];
+    for (int i = 0; i <= radius; i++) image[i] = new bool[radius+1];
+
+    rasterizeArc(radius/2);
+    rasterizeArc(radius/3);
+
+    char filename[50];
+#ifdef _WIN32
+    sprintf_s(filename, 50, "circle%d.ppm", radius);
+#else
+    sprintf(filename, "circle%d.ppm", radius);
+#endif
+
+    ofstream outfile(filename);
+    outfile << "P3\n# " << filename << "\n";
+    outfile << radius+1 << ' ' << radius+1 << ' ' << 1 << endl;
+
+    for (int i = 0; i <= radius; i++)
+    for (int j = 0; j <= radius; j++)
+        outfile << image[radius-i][j] << " 0 0\n";
+
+    // delete image data
+    for (int i = 0; i <= radius; i++) delete [] image[i];
+    delete [] image;
+
+    return 0;
+}
